dc468a37df31c5afa7da1ea25e3e24cac2ab0d9c
Georgy-Volynskiy <126697927+Georgy-Volynskiy@users.noreply.github.com>
2023-04-28 13:24:23 +0300

    Задание выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,22 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean0 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat0 = applicationContext.getBean("cat", Cat.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("   equals:");
+        System.out.println("1. bean & bean1 = " + (bean0 == bean1));
+        System.out.println("2. cat & cat1 = " + (cat0 == cat1));
+
+//      System.out.println(cat0.getMessage());
     }
 }
